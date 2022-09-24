b4496dcdbb157270bb040cc6142e392a709d6191
mao <1296193245@qq.com>
2022-09-24 21:47:08 +0800

    android_DatePicker

---
+++ app/src/main/java/mao/android_datepicker/MainActivity4.java
@@ -0,0 +1,38 @@
+package mao.android_datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity4 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        DatePicker datePicker = findViewById(R.id.DatePicker3);
+        TextView textView = findViewById(R.id.result3);
+        Button button = findViewById(R.id.button_DatePicker3);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v)
+            {
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth();
+                int dayOfMonth = datePicker.getDayOfMonth();
+                textView.setText("当前选择的是" + year + "年" + month + "月" + dayOfMonth + "日");
+            }
+        });
+    }
+}
